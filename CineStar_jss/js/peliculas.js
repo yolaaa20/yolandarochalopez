51c3531e6e7b223e5f36fa896b4cc9e98f8dfa00
yolaaa20 <117656094+yolaaa20@users.noreply.github.com>
2022-12-19 10:25:13 -0500

    Add files via upload

---
+++ CineStar_jss/js/peliculas.js
@@ -0,0 +1,25 @@
+
+
+
+/*
+<br><h1>Cartelera</h1><br>
+
+				<div class="contenido-pelicula">
+					<div class="datos-pelicula">
+						<h2>Locos de Amor 2 (+14) (HD - Doblada)</h2><br>
+						<p>¡Vuelve la comedia musical del año! Un hombre descorazonado que empieza a salir con una conductora que da consejos de amor en la radio. Una mujer desesperada por ser mamá ...</p>
+						<br>
+                       	<div class="boton-pelicula"> 
+                       		<a href="http://www.cinestar.com.pe/cartelera/pelicula/Locos-de-Amor-2" >
+                       			<img src="img/varios/btn-mas-info.jpg" width="120" height="30" alt="Ver info">
+                       		</a>
+               			</div>
+               			<div class="boton-pelicula"> 
+               				<a href="http://www.cinestar.com.pe/popups/trailer/v3fspveODBI" onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )" >
+               					<img src="img/varios/btn-trailer.jpg" width="120" height="30" alt="Ver trailer">
+               				</a>
+                        </div> 
+					</div>
+					<img src="img/pelicula/10.jpg" width="160" height="226"><br><br>
+				</div>
+*/
